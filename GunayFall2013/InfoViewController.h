52b9835fc3a8544e87c12d3a041df69874c460c3
Chris Hill <chrishill@chill100.local>
2014-01-26 14:29:22 -0500

    first commit

---
+++ GunayFall2013/InfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  InfoViewController.h
+//  GunayFall2013
+//
+//  Created by Chris Hill on 1/25/14.
+//  Copyright (c) 2014 Chris Hill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface InfoViewController : UIViewController
+
+@end
---
+++ GunayFall2013/InfoViewController.m
@@ -0,0 +1,38 @@
+//
+//  InfoViewController.m
+//  GunayFall2013
+//
+//  Created by Chris Hill on 1/25/14.
+//  Copyright (c) 2014 Chris Hill. All rights reserved.
+//
+
+#import "InfoViewController.h"
+
+@interface InfoViewController ()
+
+@end
+
+@implementation InfoViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
